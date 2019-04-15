8b317b392dd835ef2833f09db734fea0ee7e8b07
Ahmad Javed <ahmadjaved97@gmail.com>
2019-04-16 00:21:56 +0530

    added template and weather api

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
-
+import requests
+from .models import City
 # Create your views here.
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=4fcdcff335132e39c6b7fd00b04903da'
+    # city = 'London'
+
+    cities = City.objects.all()
+    weather_data = []
+
+    for city in cities:
+
+        r = requests.get(url.format(city)).json()
+        
+        city_weather = {
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description':r['weather'][0]['description'],
+            'icon':r['weather'][0]['icon'],
+        }
+
+        weather_data.append(city_weather)
+    context = {
+        'weather_data':weather_data,
+    }
+    return render(request,'weather/weather.html',context=context)
