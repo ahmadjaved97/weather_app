fdfb96cea8b9b9c46765f345eb25459916c5cbf8
Ahmad Javed <ahmadjaved97@gmail.com>
2019-07-27 15:07:31 +0530

    Updated the index function to handle wrong cities name

--- weather/views.py
+++ weather/views.py
@@ -2,42 +2,35 @@ from django.shortcuts import render
 import requests
 from .models import City
 from .forms import CityForm
+from django.http import HttpResponse
+import json
 # Create your views here.
 
+
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=4fcdcff335132e39c6b7fd00b04903da'
-    # city = 'London'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=4fcdcff335132e39c6b7fd00b04903da'
     form = CityForm(request.POST)
     if request.method == 'POST':
-        if form.is_valid:
+        if form.is_valid() and requests.get(url.format(request.POST["name"])):
             form.save()
         
-
-    
-    
     form = CityForm()
-
     cities = City.objects.all()
     weather_data = []
-
     for city in cities:
-        try:
-
-            r = requests.get(url.format(city)).json()
-        except:
-            pass
-        
+        r = requests.get(url.format(city)).json()
         city_weather = {
-            'city' : city.name,
-            'temperature' : r['main']['temp'],
-            'description' : r['weather'][0]['description'],
-            'icon' : r['weather'][0]['icon'],
-            }
+                'city' : city.name,
+                'temperature' : r['main']['temp'],
+                'description' : r['weather'][0]['description'],
+                'icon' : r['weather'][0]['icon'],
+                }
 
         weather_data.append(city_weather)
-    context = {
-        'weather_data':weather_data,
-        'form':form,
-    }
-
+        context = {
+            'weather_data':weather_data,
+            'form':form,
+        }
+    
     return render(request,'weather/weather.html',context=context)
+   
